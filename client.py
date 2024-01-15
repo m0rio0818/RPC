7897cc2432ec30b4174181bfbb5d3a348ca792b9
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-01-15 11:30:50 +0900

    client, serverno 実装”

--- client.py
+++ client.py
@@ -1,56 +1,47 @@
 import socket
 import sys
-import json
+
 
 class Client:
     def __init__(self) -> None:
+        self.address ="./rpc_server_address"
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        self.server_address = "./rpc_server_address"
         
     def start(self):
-        print("Connecting to {}".format(self.server_address))
-        try:
-            self.sock.connect(self.server_address)
-            print("Connected!")
-        except socket.error as e:
-            print("errorrr!!!!!!!!! => ",e)
-            sys.exit(1)
-        self.sendAndRecive()
+        self.connect()
+        self.sendMessage()
             
-    def sendAndRecive(self):
-        message = {
-            "method": "subtract", 
-            "params": [42, 23], 
-            "param_types": ["int", "int"],
-            "id": 1
-        }
-        
-        message_json = json.dumps(message)
-        b_message = bytes(message_json, "utf-8")
-        print(b_message)
-
-        self.sock.sendall(b_message)
-        self.sock.settimeout(2)
-        
+    def connect(self):
         try:
-            while True:
-                data = str(self.sock.recv(32))
-                if data:
-                    print("Server response :", data)
-                else:
-                    break
-        except TimeoutError:
-            print("Socket timeout, ending listening gor server messages")
-        self.sock.close()
-          
-          
-    def close(self):
-        print("Closing socket")
-        self.sock.close()
-        
-        
+            self.sock.connect(self.address)
+        except socket.error as err:
+            print(err)
+            sys.exit(1)
+            
+    def sendMessage(self):
+        try:
+            message = b"Message from client"
+            self.sock.sendall(message)
+            self.sock.settimeout(2)
+            
+            try:
+                while True:
+                    data = str(self.sock.recv(32))
+                    if data:
+                        print("Server response :", data)
+                    else:
+                        break
+            except TimeoutError:
+                print('Socket timeout, ending listening for server messages')
+        finally:
+            print("Closing socket")
+            self.sock.close()
+            
+                   
+                   
+                   
 def main():
-    client = Client()
+    client = Client();
     client.start()
     
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,75 +1,49 @@
 import socket
 import os
-import math
 
-class RPC:
-    def __init__(self, method, params, param_types, id) -> None:
-        self.method = method
-        self.params = params
-        self.param_tyes= param_types
-        self.id = id
-        
-    def substract(self, int1, int2):
-        return int1-int2
-        
-    def floor(self, x):
-        return math.floor(x)
-    
-    def nroot(self, n, x):
-        return math.pow(x, 1/n)
-    
-    def reverse(self, s):
-        return "".join(list(reversed(s)))
-
-    def validAnagram(self, str1, str2):
-        return lambda params: set(str1) == set(str2)
-    
-    def sort(self, strArr) -> None:
-        return strArr.sort()
-        
 class Server:
     def __init__(self) -> None:
+        self.address = "./rpc_server_address"
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        self.server_address = "./rpc_server_address"
-        
+    
+    
     def start(self):
         try:
-            os.unlink(self.server_address)
+            os.unlink(self.address)
         except FileNotFoundError:
             pass
-        print("Starting up on {}".format(self.server_address))
-        self.accept()
-        self.sendAndRecive()
+        print("Starting up on {}".format(self.address))
+        self.connect()
+        self.getMessage()
         
-    def accept(self):
-        self.sock.bind(self.server_address)
-        self.sock.listen(10)
         
-    def sendAndRecive(self):
+    def connect(self):
+        self.sock.bind(self.address)
+        self.sock.listen(1)
+    
+    def getMessage(self):
         while True:
             connection, client_address = self.sock.accept()
             try:
-                print("Connection from ", client_address)
-                
+                print("Connection from".format(client_address))
                 while True:
                     data = connection.recv(32)
                     data_str = data.decode("utf-8")
-                    print("Recived :", data_str)
-                    
+                    print("Recived : " + data_str)
                     if data:
-                        response = "Processing " + data_str
+                        response = "Processing : " + data_str
                         connection.sendall(response.encode())
                     else:
-                        print("no data from ", client_address)
+                        print("No data from", client_address)
                         break
             finally:
                 print("Closing current connection")
                 connection.close()
-
-        
+                
+                
 def main():
     server = Server()
     server.start()
-        
+    
 if __name__ == "__main__":
     main()
